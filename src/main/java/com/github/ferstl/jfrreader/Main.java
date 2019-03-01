4fa678b11dc33dd08a177e10a3bb12b0ac55a679
Stefan Ferstl <st.ferstl@gmail.com>
2019-03-01 07:05:52 +0100

    Get FlightRecorderMXBean

---
+++ src/main/java/com/github/ferstl/jfrreader/Main.java
@@ -0,0 +1,42 @@
+package com.github.ferstl.jfrreader;
+
+import java.io.IOException;
+import java.util.Map;
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import jdk.management.jfr.FlightRecorderMXBean;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // Use the PID of a running JVM
+    String pid = args[0];
+    FlightRecorderMXBean flightRecorder = getFlightRecorder(pid);
+    long streamId = flightRecorder.openStream(1, Map.of());
+
+
+    //    for (byte[] bytes = flightRecorder.readStream(streamId); bytes != null; bytes = flightRecorder.readStream(streamId)) {
+    //      System.out.println("Read " + bytes.length + " bytes");
+    //    }
+
+  }
+
+  private static FlightRecorderMXBean getFlightRecorder(String pid) throws AttachNotSupportedException, IOException, MalformedObjectNameException {
+    VirtualMachine vm = VirtualMachine.attach(pid);
+
+    String jmxServiceUrl = vm.startLocalManagementAgent();
+    JMXConnector jmxConnector = JMXConnectorFactory.connect(new JMXServiceURL(jmxServiceUrl));
+    MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
+
+    ObjectName objectName = new ObjectName("jdk.management.jfr:type=FlightRecorder");
+    return JMX.newMXBeanProxy(mBeanServerConnection, objectName, FlightRecorderMXBean.class);
+  }
+
+}

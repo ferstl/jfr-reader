01d16aa6fad250c709f2e24259bad40df344faf0
Stefan Ferstl <st.ferstl@gmail.com>
2019-03-02 09:03:40 +0100

    First attempt to read a flight recording

--- src/main/java/com/github/ferstl/jfrreader/Main.java
+++ src/main/java/com/github/ferstl/jfrreader/Main.java
@@ -1,5 +1,6 @@
 package com.github.ferstl.jfrreader;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Map;
 import javax.management.JMX;
@@ -9,6 +10,14 @@ import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
+import org.openjdk.jmc.common.IDisplayable;
+import org.openjdk.jmc.common.item.Aggregators;
+import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.common.item.ItemFilters;
+import org.openjdk.jmc.common.unit.IQuantity;
+import org.openjdk.jmc.flightrecorder.JfrAttributes;
+import org.openjdk.jmc.flightrecorder.JfrLoaderToolkit;
+import org.openjdk.jmc.flightrecorder.jdk.JdkTypeIDs;
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 import jdk.management.jfr.FlightRecorderMXBean;
@@ -24,11 +33,20 @@ public class Main {
     long clonedRecording = cloneRecording(flightRecorder, recordingId);
 
     long streamId = flightRecorder.openStream(clonedRecording, Map.of());
+    byte[] data = new byte[0];
     for (byte[] bytes = flightRecorder.readStream(streamId); bytes != null; bytes = flightRecorder.readStream(streamId)) {
-      System.out.println("Read " + bytes.length + " bytes");
+      data = concat(data, bytes);
     }
 
     flightRecorder.closeRecording(clonedRecording);
+
+    IItemCollection events = JfrLoaderToolkit.loadEvents(new ByteArrayInputStream(data));
+    IQuantity aggregate = events.apply(ItemFilters.type(JdkTypeIDs.MONITOR_ENTER))
+        .getAggregate(Aggregators.stddev(JfrAttributes.DURATION));
+
+    System.out.println("The standard deviation for the Java monitor enter events was "
+        + aggregate.displayUsing(IDisplayable.AUTO));
+
 
   }
 
@@ -52,6 +70,14 @@ public class Main {
     flightRecorder.setRecordingOptions(clonedRecording, Map.of("name", "jfr stream clone of " + name));
 
     return clonedRecording;
+  }
+
+  private static byte[] concat(byte[] a, byte[] b) {
+    byte[] concatenated = new byte[a.length + b.length];
+    System.arraycopy(a, 0, concatenated, 0, a.length);
+    System.arraycopy(b, 0, concatenated, a.length, b.length);
+
+    return concatenated;
   }
 
 }

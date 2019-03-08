c87f549de6062e04da5004dec5590a3db255a2ae
Stefan Ferstl <st.ferstl@gmail.com>
2019-03-08 10:51:58 +0100

    Write GC Pauses to InfluxDB

--- src/main/java/com/github/ferstl/jfrreader/EventReader.java
+++ src/main/java/com/github/ferstl/jfrreader/EventReader.java
@@ -2,15 +2,18 @@ package com.github.ferstl.jfrreader;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.openjdk.jmc.common.IDisplayable;
+import java.util.concurrent.TimeUnit;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
 import org.openjdk.jmc.common.item.IItem;
 import org.openjdk.jmc.common.item.IItemCollection;
 import org.openjdk.jmc.common.item.IItemIterable;
 import org.openjdk.jmc.common.item.IType;
-import org.openjdk.jmc.common.unit.IQuantity;
 import org.openjdk.jmc.common.unit.UnitLookup;
 import org.openjdk.jmc.flightrecorder.JfrAttributes;
 import org.openjdk.jmc.flightrecorder.JfrLoaderToolkit;
+import org.openjdk.jmc.flightrecorder.jdk.JdkAttributes;
 import org.openjdk.jmc.flightrecorder.jdk.JdkFilters;
 
 public class EventReader {
@@ -18,18 +21,38 @@ public class EventReader {
   public static void main(String[] args) throws Exception {
     Path recording = Paths.get(args[0]);
 
+    InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "jfr", "jfr");
+    String dbName = "jfr";
+    influxDB.setDatabase(dbName);
+
     IItemCollection events = JfrLoaderToolkit.loadEvents(recording.toFile()).apply(JdkFilters.GC_PAUSE);
 
     for (IItemIterable eventTypeEntry : events) {
       IType<IItem> type = eventTypeEntry.getType();
 
       for (IItem item : eventTypeEntry) {
-        IQuantity member = JfrAttributes.DURATION.getAccessor(type).getMember(item);
-        System.out.println(member.clampedLongValueIn(UnitLookup.NANOSECOND) + " -> " + member.displayUsing(IDisplayable.AUTO));
+        long gcId = JdkAttributes.GC_ID.getAccessor(type).getMember(item).longValue();
+        String gcName = JdkAttributes.GC_NAME.getAccessor(type).getMember(item);
+        long startTimeNano = JfrAttributes.START_TIME.getAccessor(type).getMember(item).clampedLongValueIn(UnitLookup.EPOCH_NS);
+        long durationUs = JfrAttributes.DURATION.getAccessor(type).getMember(item).clampedLongValueIn(UnitLookup.MICROSECOND);
+
+        write(influxDB, gcId, gcName, startTimeNano, durationUs);
       }
     }
 
+    influxDB.close();
     System.out.println(events);
+  }
+
+  private static void write(InfluxDB influxDB, long gcId, String gcName, long startTimeNano, long durationUs) {
+    influxDB.write(Point.measurement("gc")
+        .time(startTimeNano, TimeUnit.NANOSECONDS)
+        .addField("gc_name", gcName)
+        .addField("gc_id", gcId)
+        .addField("pause_time_us", durationUs)
+        .tag("application", "jfr-test")
+        .tag("event_type", "gc_pause")
+        .build());
   }
 
 }

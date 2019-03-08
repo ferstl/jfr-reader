3581f3710eb6ade6af792321b2fd953d72c1140a
Stefan Ferstl <st.ferstl@gmail.com>
2019-03-08 19:11:16 +0100

    Remove noOpEventRecorder

--- src/main/java/com/github/ferstl/jfrreader/EventRecorder.java
+++ src/main/java/com/github/ferstl/jfrreader/EventRecorder.java
@@ -2,7 +2,6 @@ package com.github.ferstl.jfrreader;
 
 import org.openjdk.jmc.common.item.IItem;
 
-@FunctionalInterface
 public interface EventRecorder {
 
   void recordEvent(long startTimeEpochNs, IItem item);
--- src/main/java/com/github/ferstl/jfrreader/EventRecorderRegistry.java
+++ src/main/java/com/github/ferstl/jfrreader/EventRecorderRegistry.java
@@ -6,12 +6,9 @@ import java.util.Optional;
 public class EventRecorderRegistry {
 
   private final Map<String, EventRecorder> recorders;
-  private final EventRecorder noOpEventRecorder;
 
   public EventRecorderRegistry(Map<String, EventRecorder> recorders) {
     this.recorders = recorders;
-    this.noOpEventRecorder = (startTimeEpochNs, item) -> {
-    };
   }
 
   public Optional<EventRecorder> getRecorder(String eventIdentifier) {
